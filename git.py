6267907206ab51e8211a8424619b4ab1199387c7
jsonhc <json_hc@163.com>
2018-12-06 10:05:41 +0800

    add git.py

--- git.py
+++ git.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python2.7
 import pexpect
+import sys
 
 child = pexpect.spawn("git push -u origin master")
-child.expect("Username*")
+child.expect("Username for 'https://github.com':")
 child.sendline("jsonhc")
-child.expect("Password*")
+child.expect("Password for 'https://jsonhc@github.com'")
 child.sendline("HHq123456*")
+child.expect("#")
 
