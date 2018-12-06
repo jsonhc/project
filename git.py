63b8fb76dc103f873a2bf60de6218d82bccb5d56
jsonhc <json_hc@163.com>
2018-12-06 11:29:28 +0800

    add git.py

---
+++ git.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python2.7
+import pexpect
+import sys
+import subprocess
+
+cmd = "git add --all"
+p = subprocess.Popen(cmd, shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+stdout, stderr = p.communicate()
+cmd1 = sys.argv[1]
+p1 = subprocess.Popen(cmd1, shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+child = pexpect.spawn("git push -u origin master")
+child.expect("Username for 'https://github.com':")
+child.sendline("jsonhc")
+child.expect("Password for 'https://jsonhc@github.com'")
+child.sendline("**********")
+child.expect("Branch master*")
+
