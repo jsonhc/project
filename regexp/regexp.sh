a78c37a461523398c06d2d57710d82994c626d67
wadeson <json_hc@163.com>
2018-12-19 17:03:02 +0800

    Update regexp.sh

--- regexp/regexp.sh
+++ regexp/regexp.sh
@@ -42,3 +42,20 @@ a1b2cbc
 
 wadeson@wadeson-Inspiron-7460:~/shell_demo/re$ cat re.sh |grep -Po "(\w)b\1"
 cbc                  # 引用\w这个匹配到的分组
+
+[root@aliyun grep]# cat access.log 
+12/May/2018:12:19:45 GET
+
+[root@aliyun grep]# cat access.log |grep -Po "12\/May\/2018(:\d{2}){3}"
+12/May/2018:12:19:45
+
+[root@aliyun grep]# cat access.log |grep -Po "\s.*(GET)"
+ GET
+[root@aliyun grep]# cat access.log |grep -Po ".*\s.*(GET)"
+12/May/2018:12:19:45 GET             # \s是指空格字符
+
+[root@aliyun grep]# cat access.log |grep -Po "12\/May\/2018(:\d+){3}"
+12/May/2018:12:19:45
+
+[root@aliyun grep]# cat access.log |grep -Po "12\/May\/2018(:)\d+\1"
+12/May/2018:12:              # 将(:)冒号作为分组，\1引用前面定义得分组
